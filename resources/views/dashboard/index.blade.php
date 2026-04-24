294462019e80c6474c2c0174e0f3798da65d9fc1
one sarumaha <onesarumaha10@gmail.com>
2026-04-24 11:44:05 +0700

    template dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,15 +9,16 @@
         />
         <title>PolluxUI Admin</title>
         <!-- base:css -->
-        <link rel="stylesheet" href="vendors/typicons/typicons.css" />
-        <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css" />
+    
+        <link rel="stylesheet" href="{{ asset('frontend-dash/template/vendors/typicons/typicons.css') }}">
+        <link rel="stylesheet" href="{{ asset('frontend-dash/template/vendors/css/vendor.bundle.base.css') }}">
         <!-- endinject -->
         <!-- plugin css for this page -->
         <!-- End plugin css for this page -->
         <!-- inject:css -->
-        <link rel="stylesheet" href="css/vertical-layout-light/style.css" />
+        <link rel="stylesheet" href="{{ asset('frontend-dash/template/css/vertical-layout-light/style.css') }}" />
         <!-- endinject -->
-        <link rel="shortcut icon" href="images/favicon.png" />
+        <link rel="shortcut icon" href="{{ asset('frontend-dash/template/images/favicon.png') }}" />
     </head>
     <body>
         <div class="row" id="proBanner">
@@ -1749,20 +1750,20 @@
         <!-- container-scroller -->
 
         <!-- base:js -->
-        <script src="vendors/js/vendor.bundle.base.js"></script>
+        <script src="{{ asset('frontend-dash/template/vendors/js/vendor.bundle.base.js')}}"></script>
         <!-- endinject -->
         <!-- Plugin js for this page-->
-        <script src="vendors/chart.js/Chart.min.js"></script>
+        <script src="{{ asset('frontend-dash/template/vendors/chart.js/Chart.min.js')}}"></script>
         <!-- End plugin js for this page-->
         <!-- inject:js -->
-        <script src="js/off-canvas.js"></script>
-        <script src="js/hoverable-collapse.js"></script>
-        <script src="js/template.js"></script>
-        <script src="js/settings.js"></script>
-        <script src="js/todolist.js"></script>
+        <script src="{{ asset('frontend-dash/template/js/off-canvas.js')}}"></script>
+        <script src="{{ asset('frontend-dash/template/js/hoverable-collapse.js')}}"></script>
+        <script src="{{ asset('frontend-dash/template/js/template.js')}}"></script>
+        <script src="{{ asset('frontend-dash/template/js/settings.js')}}"></script>
+        <script src="{{ asset('frontend-dash/template/js/todolist.js')}}"></script>
         <!-- endinject -->
         <!-- Custom js for this page-->
-        <script src="js/dashboard.js"></script>
+        <script src="{{ asset('frontend-dash/template/js/dashboard.js')}}"></script>
         <!-- End custom js for this page-->
     </body>
 </html>
